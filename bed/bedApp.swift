9caf88fd3b4aa755d4a0f7cc1a6ba769376b89a7
ChrisMcElroy <csmcelroy@me.com>
2022-10-09 00:57:58 +0100

    Initial Commit

---
+++ bed/bedApp.swift
@@ -0,0 +1,17 @@
+//
+//  bedApp.swift
+//  bed
+//
+//  Created by Chris McElroy on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct bedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
